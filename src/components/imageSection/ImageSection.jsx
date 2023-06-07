ec2e7f98afb02d0ab94dacf561286800504e24bf
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2023-06-08 01:32:57 +0300

    image section done!

---
+++ src/components/imageSection/ImageSection.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './ImageSection.css';
+import TransButton from '../buttons/TransButton';
+
+const ImageSection = ({ image, title, desc, btn }) => {
+  return (
+    <div className='flex relative mb-10'>
+      <img
+        src={image}
+        alt='background'
+        className='w-full h-full object-cover'
+      />
+      <div className='absolute w-full h-full image-shadow'></div>
+      <div className=' w-full center flex justify-center flex-col text-white container mx-auto'>
+        <h6 className='tracking-wider uppercase text-sm font-light'>{title}</h6>
+        <h1 className='tracking-wide text-7xl uppercase font-light my-6 max-w-4xl'>
+          {desc}
+        </h1>
+
+        <TransButton title={btn} />
+      </div>
+    </div>
+  );
+};
+
+export default ImageSection;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import Hero from '../../components/hero/Hero';
 import Reservation from '../../components/reservation/Reservation';
+import ImageSection from '../../components/imageSection/ImageSection';
+import carBackgrounds from '../../asset/yuzde-yuz-elektrikli-surus-keyfi--main-banner-2560x852-1_3to1.webp';
+import joyBlog from '../../asset/JoyBlog.webp';
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Reservation />
+      <ImageSection
+        title='tamamen elektrikli bmw modelleri.'
+        image={carBackgrounds}
+        desc='%100 elektrikli sürüş keyfi.'
+        btn='Modelleri keşfedin'
+      />
+
+      <ImageSection
+        title='bmw joy blog'
+        image={joyBlog}
+        desc=' BMW dünyasından en son gelişmeler, ilham veren yazılar ve çok daha fazlası. '
+        btn='Keşfedin'
+      />
     </div>
   );
 };
